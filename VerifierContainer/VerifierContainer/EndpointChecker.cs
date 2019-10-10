a154e89751417f9ce8ee34eed259913d7952f2d3
Artur Upton Renault <aruptonr@microsoft.com>
2019-10-10 16:38:48 -0700

    better error message

--- VerifierContainer/VerifierContainer/EndpointChecker.cs
+++ VerifierContainer/VerifierContainer/EndpointChecker.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace VerifierContainer
 {
     public static class EndpointChecker
     {
-        public static string GetEndpoint(string host, int port, int retries = 0)
+        public static string GetEndpoint(string host, int port, TimeSpan timeout = default, int retries = 0)
         {
+            if(timeout == default)
+            {
+                timeout = TimeSpan.FromSeconds(5);
+            }
+
             int attempts = 0;
             while (true)
             {
@@ -17,25 +23,26 @@ namespace VerifierContainer
 
                 try
                 {
-                    s.Connect(host, port);
+                    IAsyncResult result = s.BeginConnect(host, port, null, null);
+                    bool success = result.AsyncWaitHandle.WaitOne((int)timeout.TotalMilliseconds, true);
 
                     if (s.Connected)
                     {
-                        return "Connected";
+                        return $"Connected to {host}:{port}";
                     }
                     else
                     {
                         attempts++;
                         if (attempts > retries)
                         {
-                            return $"Failed to connect.";
+                            return $"Failed to connect to {host}:{port}. Timed out after {timeout.TotalMilliseconds} milliseconds.";
                         }
                         continue;
                     }
                 }
                 catch (Exception e)
                 {
-                    return $"Failed to connect. Exception: {e}";
+                    return $"Failed to connect to {host}:{port}. Exception: {e}";
                 }
                 finally
                 {
